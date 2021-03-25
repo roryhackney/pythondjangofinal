6b8e147623138c118f9bc1ee7cb1f36c0553bcd0
roryhackney <roryhackney@gmail.com>
2021-03-24 17:46:49 -0700

    models, migration, superuser

--- goals/admin.py
+++ goals/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Category, Goal, Step, Reward, Profile
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Goal)
+admin.site.register(Step)
+admin.site.register(Reward)
+admin.site.register(Profile)
