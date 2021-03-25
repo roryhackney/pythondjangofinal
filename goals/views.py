7ce715608b239116e9c60e7735883b85191974f4
roryhackney <roryhackney@gmail.com>
2021-03-24 19:53:07 -0700

    Data View: Categories, Rewards

--- goals/views.py
+++ goals/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Category, Goal, Step, Reward, Profile
 
 # Create your views here.
 def index(request):
     return render(request, 'goals/index.html')
+
+def getrewards(request):
+    rewardlist=Reward.objects.all()
+    return render(request, 'goals/rewards.html' , {'rewardlist' : rewardlist})
+
+def getcategories(request):
+    categorylist=Category.objects.all()
+    return render(request, 'goals/categories.html' ,{'categorylist' : categorylist})
